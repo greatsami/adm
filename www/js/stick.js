7cb80abd220f554747fe19e1b72b82fbf43bc55a
suhib5@gmail.com <suhib5@gmail.com>
2015-03-15 14:06:59 +0300

    Upload New Design From Suhib

---
+++ www/js/stick.js
@@ -0,0 +1,21 @@
+﻿$(document).ready(function () {
+var stickyNavTop = $('.header-continer').offset().top;
+
+
+var stickyNav = function () {
+var scrollTop = $(window).scrollTop(); 
+
+
+if (scrollTop > stickyNavTop) {
+    $('.header-continer').addClass('sticky');
+} else {
+    $('.header-continer').removeClass('sticky');
+}
+};
+
+stickyNav();
+
+$(window).scroll(function () {
+stickyNav();
+});
+});
